e319071dbf5ab59dcb4f187bd1038e9cd8d713cd
Nitesh Balusu <niteshbalusu@icloud.com>
2025-09-03 09:51:51 -0400

    Bump server to alpha.19

--- server/start.sh
+++ server/start.sh
@@ -4,10 +4,10 @@ set -e
 export RUST_BACKTRACE=1
 
 # Check if the mnemonic file exists. If not, run the create command.
-if [ ! -f "/data/aspd/mnemonic" ]; then
-  echo "Creating new config at ${ASPD_CONFIG_PATH}"
-  /usr/local/bin/aspd --config "${ASPD_CONFIG_PATH}" create
+if [ ! -f "/data/captaind/mnemonic" ]; then
+  echo "Creating new config at ${CAPTAIND_CONFIG_PATH}"
+  /usr/local/bin/captaind --config "${CAPTAIND_CONFIG_PATH}" create
 fi
 
-echo "Booting aspd..."
-/usr/local/bin/aspd --config "${ASPD_CONFIG_PATH}" start
+echo "Booting captaind..."
+/usr/local/bin/captaind --config "${CAPTAIND_CONFIG_PATH}" start
